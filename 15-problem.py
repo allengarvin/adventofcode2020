f53bc2637336f04418228b1673c7950d87871acc
Allen Garvin <aurvondel@gmail.com>
2020-12-15 01:14:02 -0600

    Deleted the inefficient part 1 of problem.

---
+++ 15-problem.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys, os, argparse, operator, re
+
+def main(args):
+    seq = map(int, args.nlist.split(","))
+    
+    previous = { n: i for i, n in enumerate(seq) }
+    
+    # can we assume the leading numbers are unique?
+    next_seq = 0
+    cnt = len(seq)
+    while True:
+        last = { next_seq:cnt }
+        next_seq = cnt - previous.get(next_seq, cnt)
+        previous.update(last)
+        cnt += 1
+
+        if cnt + 1 == 2020:
+            print "Problem 1:", next_seq
+        if cnt + 1 == 30000000:
+            print "Problem 2:", next_seq
+            break
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser(description="2020 Day 15 AOC: Rambunctious recitation")
+    ap.add_argument("nlist", help="Numberlist", default="0,3,6",nargs="?")
+    args = ap.parse_args()
+    main(args)
+    
