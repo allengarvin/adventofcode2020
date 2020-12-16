85754f0a1b6e114d2596d5f6e14e26152960dd59
Allen Garvin <aurvondel@gmail.com>
2020-12-16 08:15:36 -0600

    Going back to clean up and make succinct

--- 01-problem1.py
+++ 01-problem1.py
@@ -2,33 +2,18 @@
 
 import sys, os, argparse
 from itertools import combinations
-import operator
+from operator import mul
 
 def main(args):
-    count_one = count_two = 0
-    with open(args.file) as fd:
-        nums = map(int, fd.read().splitlines())
+    nums = map(int, open(args.file).readlines())
 
-        for a in combinations(nums, 2):
-            if sum(a) == 2020:
-                count_one = reduce(operator.mul, a, 1)
-        for a in combinations(nums, 3):
-            if sum(a) == 2020:
-                count_two = reduce(operator.mul, a, 1)
-    if args.one:
-        print("Problem 1: %d" % count_one)
-    if args.two:
-        print("Problem 2: %d" % count_two)
-
+    for n in [2,3]:
+        print "Problem {0}: {1}".format(n-1, reduce(mul, filter(lambda x: sum(x) == 2020, combinations(nums, n))[0]))
 
 if __name__ == "__main__":
     default_file = sys.argv[0].split("-")[0] + "-input.txt"
-    ap = argparse.ArgumentParser(description="2020 Day 1 AOC")
-    ap.add_argument("-1", "--one", action="store_true", help="Problem 1")
-    ap.add_argument("-2", "--two", action="store_true", help="Problem 2")
+    ap = argparse.ArgumentParser(description="2020 Day 1 AOC: Report repair")
     ap.add_argument("file", help="Input file", default=default_file, nargs="?")
     args = ap.parse_args()
-    if not args.one and not args.two:
-        args.one = args.two = True
     main(args)
     
--- 02-problem2.py
+++ 02-problem2.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python
 
 import sys, os, argparse
+from parse import parse
+from operator import xor
 
 def main(args):
-    fd = open(args.file)
-    count_one = count_two = 0
-
-    for line in fd:
-        r, let, p = line.strip().split()
-        let = let.strip(":")
-        low, high = map(int, r.split("-"))
-        if p.count(let) >= low and p.count(let) <= high:
-            count_one += 1            
-        num = 0
-        if p[low-1] == let:
-            num += 1
-        if p[high-1] == let:
-            num += 1
-        if num == 1:
-            count_two += 1
-
-    if args.one:
-        print("Problem 1: %d" % count_one)
-    if args.two:
-        print("Problem 2: %d" % count_two)
+    a, b = 0, 0
+    for line in open(args.file).readlines():
+        p = parse("{low:d}-{high:d} {let:1w}: {passwd:w}\n", line)
+        ns = argparse.Namespace(**p.__dict__["named"])
+        a += ns.low <= ns.passwd.count(ns.let) <= ns.high
+        b += xor(ns.passwd[ns.low-1] == ns.let, ns.passwd[ns.high-1] == ns.let)
+    print("Problem 1: {0}".format(a))
+    print("Problem 2: {0}".format(b))
 
 if __name__ == "__main__":
     default_file = sys.argv[0].split("-")[0] + "-input.txt"
     ap = argparse.ArgumentParser(description="2020 Day 2 AOC: Password philosophy")
-    ap.add_argument("-1", "--one", action="store_true", help="Problem 1")
-    ap.add_argument("-2", "--two", action="store_true", help="Problem 2")
     ap.add_argument("file", help="Input file", default=default_file, nargs="?")
-    args = ap.parse_args()
-    if not args.one and not args.two:
-        args.one = args.two = True
-    main(args)
+    main(ap.parse_args())
     
