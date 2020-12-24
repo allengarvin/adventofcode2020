ed3d15d308955e4f67ff536c21878c5315da5321
Allen Garvin <aurvondel@gmail.com>
2020-12-24 01:34:23 -0600

    Changed to floats (thanks boucher! these work great)

--- 24-problem.py
+++ 24-problem.py
@@ -1,52 +1,34 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3.7
 
-import sys, os, argparse, operator, re
-from parse import parse
-
-def tuple_add(t1, t2):
-    return tuple(map(lambda i, j: i + j, t1, t2))
-
-def parse_line(h):
-    dirs = []
-    i = 0
-    while i < len(h):
-        if h[i:i+2] in ["ne", "se", "sw", "nw"]:
-            dirs.append(h[i:i+2])
-            i += 2
-        else:
-            dirs.append(h[i])
-            i += 1
-    return dirs
+import sys, os, argparse, re
 
 def main(args):
     floor = dict()
-    tiles = [parse_line(x.strip()) for x in open(args.file)]
-    shifts = { "ne" : (1,-1), "se" : (1,1), "nw" : (-1,-1), "sw" : (-1,1), "w" : (-2,0), "e" : (2, 0) }
+    tiles = [re.findall("[ns][ew]|e|w", h) for h in open(args.file)]
+    shifts_c = { "ne" : 1 -1j, "se" : 1 + 1j, "nw" : -1 - 1j, "sw" : -1 + 1j, "w" : -2, "e" : 2 }
 
     for t in tiles:
-        x, y = reduce(lambda x,y: tuple_add(x,y), [shifts[x] for x in t])
-        floor[(x,y)] = 1 - floor.get((x,y), 0)
+        d = sum([shifts_c[x] for x in t])
+        floor[d] = 1 - floor.get(d, 0)
     
-    print("Problem 1: {0}".format(floor.values().count(1)))
+    print("Problem 1: {0}".format(sum(floor.values())))
 
-    for day in range(1, 101):
+    for day in range(100):
         new_floor = dict()
-        for x,y in floor.keys():
-            new_floor[(x,y)] = floor.get((x,y), 0)
-            neighbors = {tuple_add((x,y), p) : floor.get(tuple_add((x,y), p), 0) for p in shifts.values()}
-            new_floor.update(neighbors)
+        new_floor.update(floor)
+        for p in floor.keys():
+            new_floor.update({ p + p1 : floor.get(p + p1, 0) for p1 in shifts_c.values() })
     
-        for i, j in new_floor:
-            if new_floor[(i,j)]:
-                black_count = [ floor.get(tuple_add((i,j), p), 0) for p in shifts.values() ].count(1)
+        for p in new_floor:
+            black_count = sum([floor.get(p + p1, 0) for p1 in shifts_c.values()])
+            if new_floor[p]:
                 if black_count == 0 or black_count > 2:
-                    new_floor[(i,j)] = 0
-            else:
-                if [ floor.get(tuple_add((i,j), p), 0) for p in shifts.values() ].count(1) == 2:
-                    new_floor[(i,j)] = 1
+                    new_floor[p] = 0
+            elif black_count == 2:
+                new_floor[p] = 1
         floor = new_floor
-    
-        #print("Day {0}: {1}".format(day, floor.values().count(1)))
+            
+        # print("Day {0}: {1}".format(day+1, sum(floor.values())))
     print("Problem 2: {0}".format(floor.values().count(1)))
 
 if __name__ == "__main__":
