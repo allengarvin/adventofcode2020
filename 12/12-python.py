f73da5ce58267b4df2948b9378a43814b7d09a45
Allen Garvin <aurvondel@gmail.com>
2021-03-01 11:14:20 -0600

    Moved from tuples to complex

--- 12/12-python.py
+++ 12/12-python.py
@@ -1,64 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import sys, os, argparse, operator, re
-from parse import parse
-import numpy as np
+from sys import argv
 
-def tuple_add(t1, t2):
-    return tuple(map(lambda i, j: i + j, t1, t2))
+def rotate(point, rot):
+    if rot > 0:
+        rot = rot - 4
+    return point * 1j**(-rot)       # complex plane rotations are great!
 
-def rotate(pos, deg):
-    th = np.radians(deg)
+def answer(p):
+    print(abs(int(p.real + p.imag)))
 
-    py, px = pos
-    qx = np.rint(np.cos(th) * px - np.sin(th) * py)
-    qy = np.rint(np.sin(th) * px + np.cos(th) * py)
-    # print "ROT", pos, "rotated", deg, "->", (qy, qx)
-    return (int(qy), int(qx))
+def main(input_file):
+    dirs = [1j, -1, -1j, 1]
+    movements = dict(zip("SWNE", dirs))
+    pos, current_dir = 0, 0
 
-def main(args):
-    dirs = [(0,1), (-1,0), (0,-1), (1,0)]
-    current_dir = 0
-    pos = (0,0)
-
-    actions = [(a[0],int(a[1:].strip())) for a in open(args.file).readlines()]
+    actions = [(a[0], (int(a[1:]) // 90) % 4 if a[0] in "LR" else int(a[1:])) for a in open(input_file).readlines()]
     for a, n in actions:
-        if a == "N": pos = tuple_add(pos, (-n, 0))
-        if a == "S": pos = tuple_add(pos, (n, 0))
-        if a == "E": pos = tuple_add(pos, (0, n))
-        if a == "W": pos = tuple_add(pos, (0, -n))
-        if a == "F": pos = tuple_add(pos, (dirs[current_dir][0] * n, dirs[current_dir][1] * n))
-        if a == "L": current_dir = (current_dir + n / 90) % 4
-        if a == "R": current_dir = (current_dir - n / 90) % 4
-        #print a, n, current_dir, pos
-    print("Problem 1: %d" % sum(map(abs, pos)))
+        if a in "NSEW":
+            pos += movements[a] * n
+        elif a == "F":
+            pos += dirs[current_dir] * n
+        elif a == "L": 
+            current_dir = (current_dir + n) % 4
+        if a == "R": 
+            current_dir = (current_dir - n) % 4
+    answer(pos)
 
-    pos = (0,0)
-    current_dir = 0
-    waypoint = (-1, 10)
+    pos, current_dir, = 0, 0
+    way = -1 + 10j
+    movements = dict(zip("SWNE", dirs[::-1]))       # a little trickery to make the problem simpler
 
     for a, n in actions:
-        if a == "N": waypoint = tuple_add(waypoint, (-n, 0))
-        if a == "S": waypoint = tuple_add(waypoint, (n, 0))
-        if a == "E": waypoint = tuple_add(waypoint, (0, n))
-        if a == "W": waypoint = tuple_add(waypoint, (0, -n))
-
-        if a == "F": pos = tuple_add(pos, (waypoint[0] * n, waypoint[1] * n))
-        if a == "L": waypoint = rotate(waypoint, -n)
-        if a == "R": waypoint = rotate(waypoint, n)
-        # print a, n, "pos=", pos, "waypoint=", waypoint
-
-    print("Problem 2: %d" % sum(map(abs, pos)))
-        #print a, n, current_dir, pos, waypoint
+        if a in "NSEW":
+            way += movements[a] * n
+        elif a == "F": 
+            pos += way * n
+        elif a == "L": 
+            way = rotate(way, -n)
+        elif a == "R": 
+            way = rotate(way, n)
+    answer(pos)
         
 if __name__ == "__main__":
-    default_file = sys.argv[0].split("-")[0] + "-input.txt"
-    ap = argparse.ArgumentParser(description="2020 Day X AOC: Rain Risk")
-    ap.add_argument("-1", "--one", action="store_true", help="Problem 1")
-    ap.add_argument("-2", "--two", action="store_true", help="Problem 2")
-    ap.add_argument("file", help="Input file", default=default_file, nargs="?")
-    args = ap.parse_args()
-    if not args.one and not args.two:
-        args.one = args.two = True
-    main(args)
+    main(argv[1] if len(argv) > 1 else argv[0].split("-")[0] + "-input.txt")
     
