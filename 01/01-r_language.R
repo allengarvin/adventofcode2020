9ef814798d76bad52150fa5a8c9fae48de872ec9
Allen Garvin <aurvondel@gmail.com>
2021-03-04 00:51:52 -0600

    quiet

--- 01/01-r_language.R
+++ 01/01-r_language.R
@@ -2,7 +2,7 @@
 
 library(purrr)
 
-numbers <- scan("01-input.txt", numeric())
+numbers <- scan("01-input.txt", numeric(), quiet=TRUE)
 for (comb in c(2, 3))
     for (n in combn(numbers, comb, simplify=FALSE)) 
         if ( sum(n) == 2020 ) 
